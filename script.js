fa6c05d1d771bae322cf255040eadfa0b4ea15d6
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-08-18 22:03:08 +1000

    Implemet integer addition

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
-const num1 = '3';
-const operation = '+';
-const num2 = '5';
+// when a character is given, concatenate the value with "previousInput".
+let previousInput = '';
+let num1;
+let operation = '';
+let num2;
+
+while (true) {
+  const input = prompt();
+  if (input === null) {
+    break;
+  }
+
+  if (isOperator(input) && num1 === undefined) {
+    num1 = previousInput;
+    operation = input;
+    previousInput = '';
+  } else if (isOperator(input) && num2 === undefined) {
+    num2 = previousInput;
+    previousInput = '';
+  } else previousInput += input;
+
+  if (num1 !== undefined && operation !== '' && num2 !== undefined) {
+    num1 = operate(num1, operation, num2);
+    num2 = undefined;
+  }
+
+  alert(`
+    previousInput: ${previousInput}
+    num1: ${num1}
+    operation: ${operation}
+    num2: ${num2}
+  `);
+
+  if (input === '=') {
+    alert(num1);
+    break;
+  }
+}
 
 /**
  * Convert given string number into number and do calculation.
  * @param {String} operandL
  * @param {String} operator
  * @param {String} operandR
+ * @returns {String}
  */
 function operate(operandL, operator, operandR) {
   if (operator === '+') {
-    return +operandL + +operandR;
+    return `${+operandL + +operandR}`;
   }
 }
 
-console.log(operate(num1, operation, num2));
+function isOperator(character) {
+  return (
+    character === '+' ||
+    character === '-' ||
+    character === '/' ||
+    character === '%' ||
+    character === '='
+  );
+}
