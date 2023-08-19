15413a1377a24ad105fd1782bbdc6e4cdd24e167
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-08-19 13:46:02 +1000

    Implement auto round function for summation

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ while (true) {
   }
 
   if (isOperator(input) && num1 === undefined) {
-    num1 = parseInt(previousInput);
+    num1 = toNumber(previousInput);
     operation = input;
     previousInput = '';
   } else if (isOperator(input) && num2 === undefined) {
-    num2 = parseInt(previousInput);
+    num2 = toNumber(previousInput);
     previousInput = '';
   } else previousInput += input;
 
   if (num1 !== undefined && operation !== '' && num2 !== undefined) {
-    num1 = operate(num1, operation, num2);
+    const result = operate(num1, operation, num2);
+    num1 = roundIfNecessary(num1, num2, result);
     num2 = undefined;
     operation = input;
   }
@@ -70,4 +71,60 @@ function operate(operandL, operator, operandR) {
 function isOperator(character) {
   const OPERATORS = ['+', '-', '/', '*', '='];
   return OPERATORS.indexOf(character) != -1;
+}
+
+/**
+ * Convert string into float or integer value
+ * @param {String} string
+ * @returns {Number}
+ */
+function toNumber(string) {
+  return string.includes('.') ? parseFloat(string) : parseInt(string);
+}
+
+/**
+ * Round the result if both operand is fraction number.
+ * This is for preventing wrong calculation result caused by rounding error.
+ * @param {Number} operand1
+ * @param {Number} operand2
+ * @param {Number} result
+ * @returns
+ */
+function roundIfNecessary(operand1, operand2, result) {
+  if (areFraction(operand1, operand2) === true) {
+    const decimalDigit = getMaxDecimalDigitLength(operand1, operand2);
+    return result.toFixed(decimalDigit);
+  }
+  return result;
+}
+
+/**
+ * See if given numbers are fraction
+ * @param {Number} a
+ * @param {Number} b
+ * @returns {Boolean}
+ */
+function areFraction(a, b) {
+  return a < 1 && 0 < a && b < 1 && 0 < b;
+}
+
+/**
+ * Returns longer decimal digit length between two floating point numbers.
+ * @param {Number} a
+ * @param {Number} b
+ * @returns
+ */
+function getMaxDecimalDigitLength(a, b) {
+  const lengthA = getDecimalDigitLength(a);
+  const lengthB = getDecimalDigitLength(b);
+  return Math.max(lengthA, lengthB);
+}
+
+/**
+ * Get the decimal digit length of the given floating point number.
+ * @param {Number} num
+ * @returns {Number}
+ */
+function getDecimalDigitLength(num) {
+  return num.toString().split('.')[1].length;
 }
